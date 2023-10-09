7501c7f9e5dbcaca855adef6e3af6b53dc2cd676
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2023-10-09 09:35:56 +0900

    day13
    함수

---
+++ day13.py
@@ -0,0 +1,28 @@
+# 7-1. 함수
+
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+
+open_account() #전달하는 값이 없음
+
+def deposit(balance, money): #입금한 함수에는 잔액과 입금하려는 금액 다 전달받아서 원하는 값 반환
+    print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance+money))
+    return balance+money
+
+def withdraw(balance,money):
+    if balance >= money: #잔액이 출금보다 많으면
+        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance-money))
+        return balance-money
+    else:
+        print("출금이 완료되지 않았습니다. 잔액은 {0}원입니다.".format(balance))
+        return balance
+def withdraw_night(balance, money): #저녁에 출금
+    commission=100 #수수료 100원
+    return commission, balance-money-commission #튜플형식?
+
+balance=0 #잔액
+balance = deposit(balance, 1000)
+# balance = withdraw(balance, 2000)
+# balance = withdraw(balance, 500)
+commission, balance = withdraw_night(balance, 500)
+print("수수료는 {0}원이며, 잔액은 {1}원입니다.".format(commission,balance))
