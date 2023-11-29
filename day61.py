08f5766ee9bf3a009476a206a4de370088f7388c
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2023-11-29 22:32:29 +0900

    day61
    온도 단위 바꾸기 실습문제

---
+++ day61.py
@@ -0,0 +1,20 @@
+# 05.온도 단위 바꾸기
+
+# 온도 변환 함수
+# 화씨 온도를 섭씨 온도로 변환해 주는 fahrenheit_to_celsius 함수는 이렇게 작성할 수 있다
+
+# 화씨 온도에서 섭씨 온도로 바꿔 주는 함수
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+temperature_list = [40, 15, 32, 64, -4, 11]
+print("화씨 온도 리스트: {}".format(temperature_list))  # 화씨 온도 출력
+
+# 리스트의 값들을 화씨에서 섭씨로 변환하는 코드
+i = 0
+while i < len(temperature_list):
+    temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]), 1)
+    i += 1
+
+print("섭씨 온도 리스트: {}".format(temperature_list))  # 섭씨 온도 출력
