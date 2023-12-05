9beebdca02679fa3e4be01d939828c8426a86b4e
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2023-12-05 19:38:02 +0900

    day68
    range 연습

---
+++ day68.py
@@ -0,0 +1,10 @@
+# 03.range 연습
+
+# 주어진 numbers라는 리스트를 for문과 range함수를 사용하여 인덱스와 원소를 출력하라
+
+numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+
+# 인덱스와 원소 출력
+# 코드를 입력하세요.
+for i in range(len(numbers)):
+    print(i, numbers[i])
