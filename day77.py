5f695db0509355de59a1d5fd5fabf1ada3429e1e
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2023-12-17 17:29:50 +0900

    day77
    readme

---
+++ day77.py
@@ -0,0 +1,26 @@
+# 09.README를 더 예쁘게
+
+# 보통 README.md 파일에는 목적이 있다
+
+# 1. 이 프로젝트가 어떤 프로젝트인지 설명해준다
+# 2. 프로그램의 주요 사용법을 알려준다
+# 3. 프로그램을 실행시키려면 어떤 사전 작업이 필요한지를 알려준다
+
+# GitHub에서는 README.md 파일을 프로젝트의 메인 화면에 보여주기 때문에
+# README.md 파일의 내용을 가독성있게 작성하는 것이 중요하다
+
+# 어떻게 하면 좀더 예쁘게 꾸밀 수 있을까?
+# 그 답은 README.md 파일의 확장자인 .md에 있다
+# 이 확장자는 markdown이라는 단어의 줄임말이다
+
+# markdown은 이 파일에 마크다운으로 내용을 작성할 수 있다는 걸 나타낸다
+# 마크다운이란 특정 규칙에 맞게, 간단한 텍스트만으로 어떤 표시를 해두면
+# 그것이 자동으로 HTML 태그로 전환되도록 약속된 문법이다
+# 그래서 단지 텍스트만으로도 내용의 디자인을 예쁘게 만들 수 있다
+
+# ###(샵 3개)도 마크다운 중 하나이다
+# 이렇게 쓰면 그 줄의 텍스트를 제목처럼 보이게 만들어주는 효과가 생긴다
+
+# https://docs.github.com/ko/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+# https://www.codeit.kr/topics/getting-started-with-data-science/lessons/975
