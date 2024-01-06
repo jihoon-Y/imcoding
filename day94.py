f82d9028d9dc766936e3185206d13fbff154aff8
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2024-01-06 23:59:11 +0900

    day94
    로또

---
+++ day94.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def generate_numbers(n):
+    numbers = []
+
+    while len(numbers) < n:
+        num = randint(1, 45)
+        if num not in numbers:
+            numbers.append(num)
+
+    return numbers
+
+
+# 테스트 코드
+print(generate_numbers(6))
