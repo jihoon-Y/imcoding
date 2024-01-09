40eb13d0da75f5cd53d9f2753c6ff0b5f7556d58
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2024-01-09 16:52:00 +0900

    day97
    당첨금 확인

---
+++ day97.py
@@ -0,0 +1,31 @@
+def count_matching_numbers(numbers, winning_numbers):
+    count = 0
+
+    for num in numbers:
+        if num in winning_numbers:
+            count = count + 1
+
+    return count
+
+
+def check(numbers, winning_numbers):
+    count = count_matching_numbers(numbers, winning_numbers[:6])
+    bonus_count = count_matching_numbers(numbers, winning_numbers[6:])
+
+    if count == 6:
+        return 1000000000
+    elif count == 5 and bonus_count == 1:
+        return 50000000
+    elif count == 5:
+        return 1000000
+    elif count == 4:
+        return 50000
+    elif count == 3:
+        return 5000
+    else:
+        return 0
+
+
+# 테스트 코드
+print(check([2, 4, 11, 14, 25, 40], [4, 12, 14, 28, 40, 41, 6]))
+print(check([2, 4, 11, 14, 25, 40], [2, 4, 10, 11, 14, 40, 25]))
