890ec0a0b76a6ad0d8db601aa40ae0b72062c232
jihoon-Y <145994581+jihoon-Y@users.noreply.github.com>
2024-01-10 22:49:05 +0900

    day98
    로또시뮬레이션 코드 하나로 합치기 원본

---
+++ day98.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+def generate_numbers(n):
+    """번호 뽑기"""
+    ...
+
+
+def draw_winning_numbers():
+    """당첨 번호 뽑기"""
+    ...
+
+
+def count_matching_numbers(numbers, winning_numbers)
+    """겹치는 번호 개수"""
+    ...
+
+
+def check(numbers, winning_numbers):
+    """당첨 확인"""        
+    ...
+
